289d693e7290967e328a2d0a98c4f09972df16d4
roman <polovtsev96@yandex.ru>
2024-07-21 16:29:25 +0300

    test tasks

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,3 +1,38 @@
 public class Test {
 
+        /*
+
+    create table products(id integer, name text)
+    create table categories(id integer, name text)
+
+    create table product_categories(product_id integer, category_id integer,
+        foreign key (product_id) references products.id,
+        foreign key (product_id) references products.id);
+
+    запрос на нахождение имен категорий, у которых как минимум 3 продукта
+
+
+
+     */
+
+    /*
+    class Category {
+    String parentId;
+    String id;
+    String name;
+}
+
+class InvertedCategory{
+    List<String> childId;
+    String id;
+    String name;
+}
+
+// categoryId -> ..
+//            -> .. -> rootCategory
+
+
+Set<String> getAllParentCategories(List<Category> categories, String categoryId) {
+     */
+
 }
